fb6cba6357e9ab400d37de9f45a5b84b688f330b
Nikita Voronchev <n.voronchev@gmail.com>
2024-04-24 12:27:58 +0300

    Support another version of Bitbucket server

--- src/content.js
+++ src/content.js
@@ -1,19 +1,33 @@
 // Copyright (c) 2023 Nikita Voronchev <n.voronchev@gmail.com>
 // MIT License
 
-function modifyElement(selector, lonOnNotFound, onFound) {
+function modifyElement(selector, onFound) {
     element = document.querySelector(selector);
     if (element) {
         onFound(element);
-    } else if (lonOnNotFound) {
+    } else {
         console.log("ERROR: Element '" + selector + "' not found");
     }
 }
 
-modifyElement("#pull-requests-container > * div:has(> div.changes)", true, (element) => {
-    element.style.height = "";
-});
+//
 
-modifyElement("#pull-requests-container > div > div > div > div > div > aside.changes-sidebar", true, (element) => {
-    element.style.width = '15%'
-});
+function modifyChangesArea(changesAreaElem) {
+    changesAreaElem.style.height = "100%";
+}
+
+[
+    "#pull-requests-container > * div:has(> div.changes)",
+    "#main > * div.commit-changes"
+].forEach((selector) => modifyElement(selector, modifyChangesArea));
+
+//
+
+function modifySidebar(sidebarElem) {
+    sidebarElem.style.width = '15%';
+}
+
+[
+    "#pull-requests-container > div > div > div > div > div > aside.changes-sidebar",
+    "#main > div > div > div > div > aside.changes-sidebar"
+].forEach((selector) => modifyElement(selector, modifySidebar));
